a9e60d48cf4b642d14839904b9636f0b5fc6df2a
Gui-hornig <guihornig@gmail.com>
2023-07-03 17:49:56 -0300

    Update main.js

--- Projeto-portfolio/assets/js/main.js
+++ Projeto-portfolio/assets/js/main.js
@@ -55,7 +55,8 @@ linkWork.forEach(l=> l.addEventListener('click', activeWork))
 let swiperTestimonial = new Swiper(".tertimonial__container",{
     spaceBetween: 24,
     loop: true,
-
+    grabCursor: true,
+    
     pagination: { 
         el: ".swiper-pagination",
         clikabla: true,
