8d05b7ff7f6b93e587e4e3f6b743e5c927ec2249
Gui-hornig <guihornig@gmail.com>
2023-07-27 16:36:12 -0300

    test

--- Projeto-portfolio/assets/js/main.js
+++ Projeto-portfolio/assets/js/main.js
@@ -102,21 +102,21 @@ const iconTheme = 'bx-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+// We obtain the current theme that the interface has by validating the ligth-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the ligth
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
   themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
+    // Add or remove the ligth / icon theme
+    document.body.classList.toggle(lightTheme)
     themeButton.classList.toggle(iconTheme)
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
